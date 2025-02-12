170300b7a55171100a340c9e506dc6a1de33180d
Leotechcoder <blabla.13leo@gmail.com>
2025-02-12 16:40:48 -0300

    ruta de production, recordar cambiar cuando se cambie de entorno

--- src/shared/infrastructure/utils/routeApi.js
+++ src/shared/infrastructure/utils/routeApi.js
@@ -1,5 +1,5 @@
 //En production Railway
-// export const routeApi = 'https://charming-vision-production.up.railway.app/api'
+export const routeApi = 'https://charming-vision-production.up.railway.app/api'
 
 //En desarrollo Localhost
-export const routeApi = 'http://localhost:3000/api'
+//export const routeApi = 'http://localhost:3000/api'
