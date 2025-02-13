872027e7348e34433c369c0c7052a16d4eaba15a
Leotechcoder <blabla.13leo@gmail.com>
2025-02-13 00:49:23 -0300

    config VITE para variables en Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000, // Puedes cambiar el puerto si es necesario
+  },
+  define: {
+    'import.meta.env.VITE_ROUTE_API': JSON.stringify(process.env.VITE_ROUTE_API),
+  },
 })
