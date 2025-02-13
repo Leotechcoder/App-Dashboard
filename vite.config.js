1e359686a979c057495edaa437782ed4e93d00ce
Leotechcoder <blabla.13leo@gmail.com>
2025-02-13 02:56:47 -0300

    actualizo por variable en vercel

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000, // Puedes cambiar el puerto si es necesario
-  }
+  define: {
+    'import.meta.env.VITE_ROUTE_API': JSON.stringify(process.env.VITE_ROUTE_API),
+  },
 })
