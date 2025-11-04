c4c55a1037132324f7b312771c0fac3d927a7d2c
Leotechcoder <blabla.13leo@gmail.com>
2025-11-04 00:00:54 -0300

    Elimino el modulo orders de la navegacion

--- src/shared/presentation/components/App.jsx
+++ src/shared/presentation/components/App.jsx
@@ -6,7 +6,6 @@ import LoginPage from "../../../users/presentation/pages/LoginPage.jsx"
 import Dashboard from "../pages/Home.jsx"
 import Contact from "../../../users/presentation/pages/Contact.jsx"
 import Products from "../../../products/presentation/pages/ProductsPage.jsx"
-import OrdersPage from "../../../orders/presentation/pages/OrdersPage.jsx"
 import { SalesDashboardView } from "@/sales/presentation/pages/SalesDashboardView.jsx"
 import PreviewPage from "@/../pruebas/PreviewPage.jsx"
 
@@ -26,7 +25,6 @@ const router = createBrowserRouter([
       { path: "home", element: <Dashboard /> },
       { path: "contact", element: <Contact /> },
       { path: "products", element: <Products /> },
-      { path: "orders", element: <OrdersPage /> },
       { path: "ventas", element: <SalesDashboardView /> },
       {path: "pruebas", element: <PreviewPage />},
       { path: "*", element: <ErrorUrl /> },
